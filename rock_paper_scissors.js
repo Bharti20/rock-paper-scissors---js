9c6c3589b65ddcf760a206113866edfe2513d32e
Bharti kumari <bhartikumari20@navgurukul.org>
2021-07-03 18:47:08 +0530

    change conditions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -7,26 +7,41 @@ let computerMoves = ['rock', 'paper', 'scissors']
 console.log()
 function main(){
     let computerChoice = computerMoves[Math.floor(Math.random()*computerMoves.length)];
+    console.log(computerChoice)
     let userChoice = input.question('Enter your choice. rock, paper , scissors ----')
     if(computerChoice ==userChoice ){
         console.log(`Game drow! because you chosen ${userChoice}, and computer also chosen ${computerChoice}`)
-    }else if(computerChoice == 'paper' && userChoice == 'rock' || computerChoice == 'rock' && userChoice == 'scissors' || computerChoice == 'scissors' && userChoice == 'paper') {
-        console.log(`Computer won! because you chosen ${userChoice}, and computer chosen ${computerChoice}`)
-    }else if(userChoice == 'paper' && computerChoice == 'rock' || userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'scissors' && computerChoice == 'paper') {
-        console.log(`${name} won! because computer chosen ${computerChoice}, and you chosen ${userChoice}`)
+    }else if(computerChoice == 'paper'){
+        if(userChoice == 'rock'){
+            console.log(`Computer won! because you chosen ${userChoice}, and computer chosen ${computerChoice}`)
+        }else{
+            console.log(`${name} won! because computer chosen ${computerChoice}, and you chosen ${userChoice}`)
+        }
+    }else if(computerChoice == 'scissors') {
+        if(userChoice == 'rock') {
+            console.log(`${name} won! because computer chosen ${computerChoice}, and you chosen ${userChoice}`)
+        }else{
+            console.log(`Computer won! because you chosen ${userChoice}, and computer chosen ${computerChoice}`)
+
+        }
+    }else if(computerChoice == 'rock') {
+        if(userChoice == 'paper') {
+            console.log(`${name} won! because computer chosen ${computerChoice}, and you chosen ${userChoice}`)
+        }else{
+            console.log(`Computer won! because you chosen ${userChoice}, and computer chosen ${computerChoice}`)
+
+        }
     }else{
         console.log('invalid input')
     }
-};
-main()
-console.log()
-while(true){
+    console.log()
     let user = input.question('do you want to play again? press yes, otherwise no ----')
     if(user == 'yes'){
         console.log()
         main()
     }else{
         console.log('Thank you for visit!')
-        break;
     }
+
 };
+main()
